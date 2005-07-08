3055c41cd309204230859a4b90040c9f83eff3ae
janderse@umich.edu <janderse@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-07-08 19:38:19 +0000

    Latest experimental JSF development - does NOT affect existing tools in any way!
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@447 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- jsf/devtool/src/java/org/sakaiproject/jsf/devtool/JSFGenerator.java
+++ jsf/devtool/src/java/org/sakaiproject/jsf/devtool/JSFGenerator.java
@@ -1,7 +1,6 @@
 /**********************************************************************************
-*
-* $Header: /cvs/sakai2/jsf/widgets/src/java/org/sakaiproject/jsf/component/PagerComponent.java,v 1.1 2005/04/27 14:30:05 janderse.umich.edu Exp $
-*
+* $URL$
+* $Id$
 ***********************************************************************************
 *
 * Copyright (c) 2003, 2004 The Regents of the University of Michigan, Trustees of Indiana University,
@@ -208,7 +207,6 @@ public class JSFGenerator
 }
 
 /**********************************************************************************
-*
-* $Header: /cvs/sakai2/jsf/widgets/src/java/org/sakaiproject/jsf/component/PagerComponent.java,v 1.1 2005/04/27 14:30:05 janderse.umich.edu Exp $
-*
+* $URL$
+* $Id$
 **********************************************************************************/
