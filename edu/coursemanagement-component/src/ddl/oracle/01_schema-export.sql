e4a722ad4a2c335397550a9a93e414bf0c296975
daisyf@stanford.edu <daisyf@stanford.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-08-06 04:28:19 +0000

    add otehr status & type table
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@1028 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- edu/coursemanagement-component/src/ddl/oracle/01_schema-export.sql
+++ edu/coursemanagement-component/src/ddl/oracle/01_schema-export.sql
@@ -1,9 +1,40 @@
+drop table CM_ENROLLMENTSTATUS_T cascade constraints;
 drop table CM_SESSION_T cascade constraints;
+drop table CM_COURSEOFFERINGTYPE_T cascade constraints;
+drop table CM_COURSESETTYPE_T cascade constraints;
+drop table CM_ENROLLMENTTYPE_T cascade constraints;
 drop table CM_CANONICALCOURSESTATUS_T cascade constraints;
+drop table CM_COURSEOFFERINGSTATUS_T cascade constraints;
+drop table CM_PARTICIPATIONSTATUS_T cascade constraints;
+drop table CM_COURSESECTIONTYPE_T cascade constraints;
+drop table CM_COURSESECTIONSTATUS_T cascade constraints;
 drop table CM_SESSIONTYPE_T cascade constraints;
+drop sequence CM_COURSESECTIONTYPE_ID_S;
+drop sequence CM_COURSEOFFERINGSTATUS_ID_S;
 drop sequence CM_CANONICALCOURSESTATUS_ID_S;
+drop sequence CM_COURSEOFFERINGTYPE_ID_S;
+drop sequence CM_ENROLLMENTSTATUS_ID_S;
+drop sequence CM_ENROLLMENTTYPE_ID_S;
 drop sequence CM_SESSION_ID_S;
+drop sequence CM_COURSESETTYPE_ID_S;
 drop sequence CM_SESSIONTYPE_ID_S;
+drop sequence CM_PARTICIPATIONSTATUS_ID_S;
+drop sequence CM_COURSESECTIONSTATUS_ID_S;
+create table CM_ENROLLMENTSTATUS_T (
+   ENROLLMENTSTATUSID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (ENROLLMENTSTATUSID)
+);
 create table CM_SESSION_T (
    SESSIONID number(19,0) not null,
    TITLE varchar2(255) not null,
@@ -17,6 +48,51 @@ create table CM_SESSION_T (
    LASTMODIFIEDBY varchar2(255) not null,
    LASTMODIFIEDDATE date not null,
    primary key (SESSIONID)
+);
+create table CM_COURSEOFFERINGTYPE_T (
+   COURSEOFFERINGTYPEID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (COURSEOFFERINGTYPEID)
+);
+create table CM_COURSESETTYPE_T (
+   COURSESETTYPEID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (COURSESETTYPEID)
+);
+create table CM_ENROLLMENTTYPE_T (
+   ENROLLMENTTYPEID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (ENROLLMENTTYPEID)
 );
 create table CM_CANONICALCOURSESTATUS_T (
    CANONICALCOURSESTATUSID number(19,0) not null,
@@ -33,6 +109,66 @@ create table CM_CANONICALCOURSESTATUS_T (
    CREATEDDATE date not null,
    primary key (CANONICALCOURSESTATUSID)
 );
+create table CM_COURSEOFFERINGSTATUS_T (
+   COURSEOFFERINGSTATUSID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (COURSEOFFERINGSTATUSID)
+);
+create table CM_PARTICIPATIONSTATUS_T (
+   PARTICIPATIONSTATUSID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (PARTICIPATIONSTATUSID)
+);
+create table CM_COURSESECTIONTYPE_T (
+   COURSESECTIONTYPEID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (COURSESECTIONTYPEID)
+);
+create table CM_COURSESECTIONSTATUS_T (
+   COURSESECTIONSTATUSID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (COURSESECTIONSTATUSID)
+);
 create table CM_SESSIONTYPE_T (
    SESSIONTYPEID number(19,0) not null,
    AUTHORITY varchar2(255) not null,
@@ -48,6 +184,14 @@ create table CM_SESSIONTYPE_T (
    CREATEDDATE date not null,
    primary key (SESSIONTYPEID)
 );
+create sequence CM_COURSESECTIONTYPE_ID_S;
+create sequence CM_COURSEOFFERINGSTATUS_ID_S;
 create sequence CM_CANONICALCOURSESTATUS_ID_S;
+create sequence CM_COURSEOFFERINGTYPE_ID_S;
+create sequence CM_ENROLLMENTSTATUS_ID_S;
+create sequence CM_ENROLLMENTTYPE_ID_S;
 create sequence CM_SESSION_ID_S;
+create sequence CM_COURSESETTYPE_ID_S;
 create sequence CM_SESSIONTYPE_ID_S;
+create sequence CM_PARTICIPATIONSTATUS_ID_S;
+create sequence CM_COURSESECTIONSTATUS_ID_S;
--- edu/coursemanagement-component/src/ddl/oracle/02_default-setting.sql
+++ edu/coursemanagement-component/src/ddl/oracle/02_default-setting.sql
@@ -86,5 +86,248 @@ insert into CM_CANONICALCOURSESTATUS_T ("CANONICALCOURSESTATUSID", "AUTHORITY", 
     1, 'site', SYSDATE , 'site' ,SYSDATE);
 
 
+insert into CM_COURSEOFFERINGSTATUS_T ("COURSEOFFERINGSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering_status.unknown' ,
+    'Unknown', NULL, '*uuid_offering_status_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGSTATUS_T ("COURSEOFFERINGSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering_status.open' ,
+    'Open', NULL, '*uuid_offering_status_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGSTATUS_T ("COURSEOFFERINGSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering_status.closed' ,
+    'Closed', NULL, '*uuid_offering_status_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGSTATUS_T ("COURSEOFFERINGSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering_status.wait_listed' ,
+    'Wait Listed', NULL, '*uuid_offering_status_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGSTATUS_T ("COURSEOFFERINGSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering_status.pending' ,
+    'Pending', NULL, '*uuid_offering_status_5',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
+
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.unknown' ,
+    'Unknown', NULL, '*uuid_offering_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.prerequisite' ,
+    'Prerequisite', NULL, '*uuid_offering_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.corequisite' ,
+    'Corequisite', NULL, '*uuid_offering_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.sequence' ,
+    'Sequence', NULL, '*uuid_offering_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.active' ,
+    'Active', NULL, '*uuid_offering_5',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.discontinued' ,
+    'Discontinued', NULL, '*uuid_offering_6',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.proposed' ,
+    'Proposed', NULL, '*uuid_offering_7',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSEOFFERINGTYPE_T ("COURSEOFFERINGTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSEOFFERINGTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'offering.unlisted' ,
+    'Unlisted', NULL, '*uuid_offering_8',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
+
+
+insert into CM_COURSESECTIONSTATUS_T ("COURSESECTIONSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section_status.unknown' ,
+    'Unknown', NULL, '*uuid_section_status_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONSTATUS_T ("COURSESECTIONSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section_status.open' ,
+    'Open', NULL, '*uuid_section_status_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONSTATUS_T ("COURSESECTIONSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section_status.closed' ,
+    'Closed', NULL, '*uuid_section_status_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONSTATUS_T ("COURSESECTIONSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section_status.wait_listed' ,
+    'Wait Listed', NULL, '*uuid_section_status_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
+
+insert into CM_COURSESECTIONTYPE_T ("COURSESECTIONTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section.unknown' ,
+    'Unknown', NULL, '*uuid_section_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONTYPE_T ("COURSESECTIONTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section.lecture' ,
+    'Lecture', NULL, '*uuid_section_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONTYPE_T ("COURSESECTIONTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section.seminar' ,
+    'Seminar', NULL, '*uuid_section_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONTYPE_T ("COURSESECTIONTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section.lab' ,
+    'Lab', NULL, '*uuid_section_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONTYPE_T ("COURSESECTIONTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section.recitation' ,
+    'Recitation', NULL, '*uuid_section_5',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESECTIONTYPE_T ("COURSESECTIONTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESECTIONTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'section.studio' ,
+    'Studio', NULL, '*uuid_section_6',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
+
+
+insert into CM_COURSESETTYPE_T ("COURSESETTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESETTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'group.unknown' ,
+    'Unknown', NULL, '*uuid_group_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESETTYPE_T ("COURSESETTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESETTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'group.major' ,
+    'Major', NULL, '*uuid_group_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESETTYPE_T ("COURSESETTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESETTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'group.minor' ,
+    'Minor', NULL, '*uuid_group_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_COURSESETTYPE_T ("COURSESETTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_COURSESETTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'group.department' ,
+    'Department', NULL, '*uuid_group_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
+
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.unknown' ,
+    'Unknown', NULL, '*uuid_enrollment_status_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.full_credit' ,
+    'Full Credit', NULL, '*uuid_enrollment_status_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.partial_credit' ,
+    'Partial Credit', NULL, '*uuid_enrollment_status_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.no_credit' ,
+    'No Credit', NULL, '*uuid_enrollment_status_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.auditing' ,
+    'Auditing', NULL, '*uuid_enrollment_status_5',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.dropped' ,
+    'Dropped', NULL, '*uuid_enrollment_status_6',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTSTATUS_T ("ENROLLMENTSTATUSID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTSTATUS_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment_status.pending' ,
+    'Pending', NULL, '*uuid_enrollment_status_7',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
+
+insert into CM_ENROLLMENTTYPE_T ("ENROLLMENTTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment.unknown' ,
+    'Unknown', NULL, '*uuid_enrollment_1',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTTYPE_T ("ENROLLMENTTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment.aggregate' ,
+    'Aggregate', NULL, '*uuid_enrollment_2',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTTYPE_T ("ENROLLMENTTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment.preliminary' ,
+    'Preliminary', NULL, '*uuid_enrollment_3',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+insert into CM_ENROLLMENTTYPE_T ("ENROLLMENTTYPEID", "AUTHORITY", "DOMAIN", "KEYWORD",
+    "DISPLAYNAME", "DESCRIPTION", "UUID", 
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" , "LASTMODIFIEDDATE" )
+    VALUES (CM_ENROLLMENTTYPE_ID_S.nextVal, 'org.sakaiproject' ,'coursemgmt' ,'enrollment.final' ,
+    'Final', NULL, '*uuid_enrollment_4',
+    1, 'site', SYSDATE , 'site' ,SYSDATE);
+
 
 commit;
