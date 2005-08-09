669b6e73da84774cbac299f25b4f548e6f2bb295
daisyf@stanford.edu <daisyf@stanford.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-08-09 02:59:36 +0000

    fixed bugs
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@1048 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- edu/coursemanagement-component/src/ddl/oracle/01_schema-export.sql
+++ edu/coursemanagement-component/src/ddl/oracle/01_schema-export.sql
@@ -1,15 +1,19 @@
 drop table CM_ENROLLMENTSTATUS_T cascade constraints;
-drop table CM_SESSION_T cascade constraints;
 drop table CM_COURSEOFFERINGTYPE_T cascade constraints;
 drop table CM_COURSESETTYPE_T cascade constraints;
-drop table CM_ENROLLMENTTYPE_T cascade constraints;
 drop table CM_CANONICALCOURSESTATUS_T cascade constraints;
 drop table CM_COURSEOFFERINGSTATUS_T cascade constraints;
-drop table CM_PARTICIPATIONSTATUS_T cascade constraints;
-drop table CM_COURSESECTIONTYPE_T cascade constraints;
 drop table CM_COURSESECTIONSTATUS_T cascade constraints;
+drop table CM_COURSESECTIONTYPE_T cascade constraints;
+drop table CM_CANONICALCOURSE_T cascade constraints;
+drop table CM_SESSION_T cascade constraints;
+drop table CM_EQUIVALENTCOURSES_T cascade constraints;
+drop table CM_ENROLLMENTTYPE_T cascade constraints;
+drop table CM_PARTICIPATIONSTATUS_T cascade constraints;
 drop table CM_SESSIONTYPE_T cascade constraints;
+drop table CM_COURSESET_T cascade constraints;
 drop sequence CM_COURSESECTIONTYPE_ID_S;
+drop sequence hibernate_sequence;
 drop sequence CM_COURSEOFFERINGSTATUS_ID_S;
 drop sequence CM_CANONICALCOURSESTATUS_ID_S;
 drop sequence CM_COURSEOFFERINGTYPE_ID_S;
@@ -34,20 +38,6 @@ create table CM_ENROLLMENTSTATUS_T (
    CREATEDBY varchar2(255) not null,
    CREATEDDATE date not null,
    primary key (ENROLLMENTSTATUSID)
-);
-create table CM_SESSION_T (
-   SESSIONID number(19,0) not null,
-   TITLE varchar2(255) not null,
-   ABBREVIATION varchar2(255) not null,
-   YEAR varchar2(255) not null,
-   ISCURRENT number(1,0) not null,
-   UUID varchar2(255) not null,
-   SESSIONTYPEUUID varchar2(255),
-   CREATEDBY varchar2(255) not null,
-   CREATEDDATE date not null,
-   LASTMODIFIEDBY varchar2(255) not null,
-   LASTMODIFIEDDATE date not null,
-   primary key (SESSIONID)
 );
 create table CM_COURSEOFFERINGTYPE_T (
    COURSEOFFERINGTYPEID number(19,0) not null,
@@ -79,21 +69,6 @@ create table CM_COURSESETTYPE_T (
    CREATEDDATE date not null,
    primary key (COURSESETTYPEID)
 );
-create table CM_ENROLLMENTTYPE_T (
-   ENROLLMENTTYPEID number(19,0) not null,
-   AUTHORITY varchar2(255) not null,
-   DOMAIN varchar2(255) not null,
-   KEYWORD varchar2(255) not null,
-   DISPLAYNAME varchar2(255),
-   DESCRIPTION varchar2(255),
-   UUID varchar2(255) not null,
-   STATUS number(10,0) not null,
-   LASTMODIFIEDBY varchar2(255) not null,
-   LASTMODIFIEDDATE date not null,
-   CREATEDBY varchar2(255) not null,
-   CREATEDDATE date not null,
-   primary key (ENROLLMENTTYPEID)
-);
 create table CM_CANONICALCOURSESTATUS_T (
    CANONICALCOURSESTATUSID number(19,0) not null,
    AUTHORITY varchar2(255) not null,
@@ -124,8 +99,8 @@ create table CM_COURSEOFFERINGSTATUS_T (
    CREATEDDATE date not null,
    primary key (COURSEOFFERINGSTATUSID)
 );
-create table CM_PARTICIPATIONSTATUS_T (
-   PARTICIPATIONSTATUSID number(19,0) not null,
+create table CM_COURSESECTIONSTATUS_T (
+   COURSESECTIONSTATUSID number(19,0) not null,
    AUTHORITY varchar2(255) not null,
    DOMAIN varchar2(255) not null,
    KEYWORD varchar2(255) not null,
@@ -137,7 +112,7 @@ create table CM_PARTICIPATIONSTATUS_T (
    LASTMODIFIEDDATE date not null,
    CREATEDBY varchar2(255) not null,
    CREATEDDATE date not null,
-   primary key (PARTICIPATIONSTATUSID)
+   primary key (COURSESECTIONSTATUSID)
 );
 create table CM_COURSESECTIONTYPE_T (
    COURSESECTIONTYPEID number(19,0) not null,
@@ -154,8 +129,44 @@ create table CM_COURSESECTIONTYPE_T (
    CREATEDDATE date not null,
    primary key (COURSESECTIONTYPEID)
 );
-create table CM_COURSESECTIONSTATUS_T (
-   COURSESECTIONSTATUSID number(19,0) not null,
+create table CM_CANONICALCOURSE_T (
+   CANONICALCOURSEID number(19,0) not null,
+   TITLE varchar2(255),
+   DESCRIPTION varchar2(255),
+   COURSENUMBER varchar2(255) not null,
+   UUID varchar2(255) not null,
+   DEFAULTCREDITS varchar2(255),
+   PARENTID varchar2(255),
+   TOPICS varchar2(255),
+   PREREQUISITE varchar2(255),
+   CANONICALCOURSESTATUSTYPEUUID varchar2(255) not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CANONICALCOURSEUUID number(19,0),
+   primary key (CANONICALCOURSEID)
+);
+create table CM_SESSION_T (
+   SESSIONID number(19,0) not null,
+   TITLE varchar2(255) not null,
+   ABBREVIATION varchar2(255) not null,
+   YEAR varchar2(255) not null,
+   ISCURRENT number(1,0) not null,
+   UUID varchar2(255) not null,
+   SESSIONTYPEUUID varchar2(255),
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   primary key (SESSIONID)
+);
+create table CM_EQUIVALENTCOURSES_T (
+   EQUIVALENTID number(19,0) not null,
+   primary key (EQUIVALENTID)
+);
+create table CM_ENROLLMENTTYPE_T (
+   ENROLLMENTTYPEID number(19,0) not null,
    AUTHORITY varchar2(255) not null,
    DOMAIN varchar2(255) not null,
    KEYWORD varchar2(255) not null,
@@ -167,7 +178,22 @@ create table CM_COURSESECTIONSTATUS_T (
    LASTMODIFIEDDATE date not null,
    CREATEDBY varchar2(255) not null,
    CREATEDDATE date not null,
-   primary key (COURSESECTIONSTATUSID)
+   primary key (ENROLLMENTTYPEID)
+);
+create table CM_PARTICIPATIONSTATUS_T (
+   PARTICIPATIONSTATUSID number(19,0) not null,
+   AUTHORITY varchar2(255) not null,
+   DOMAIN varchar2(255) not null,
+   KEYWORD varchar2(255) not null,
+   DISPLAYNAME varchar2(255),
+   DESCRIPTION varchar2(255),
+   UUID varchar2(255) not null,
+   STATUS number(10,0) not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   primary key (PARTICIPATIONSTATUSID)
 );
 create table CM_SESSIONTYPE_T (
    SESSIONTYPEID number(19,0) not null,
@@ -184,7 +210,19 @@ create table CM_SESSIONTYPE_T (
    CREATEDDATE date not null,
    primary key (SESSIONTYPEID)
 );
+create table CM_COURSESET_T (
+   COURSESETID number(19,0) not null,
+   TITLE varchar2(255),
+   UUID varchar2(255) not null,
+   CREATEDBY varchar2(255) not null,
+   CREATEDDATE date not null,
+   LASTMODIFIEDBY varchar2(255) not null,
+   LASTMODIFIEDDATE date not null,
+   CANONICALCOURSEUUID number(19,0),
+   primary key (COURSESETID)
+);
 create sequence CM_COURSESECTIONTYPE_ID_S;
+create sequence hibernate_sequence;
 create sequence CM_COURSEOFFERINGSTATUS_ID_S;
 create sequence CM_CANONICALCOURSESTATUS_ID_S;
 create sequence CM_COURSEOFFERINGTYPE_ID_S;
--- edu/coursemanagement-component/src/test/org/sakaiproject/component/common/edu/coursemanagement/test/CourseManagerTest.java
+++ edu/coursemanagement-component/src/test/org/sakaiproject/component/common/edu/coursemanagement/test/CourseManagerTest.java
@@ -1,26 +1,26 @@
 /**********************************************************************************
-*
-* $Id$
-*
-***********************************************************************************
-*
-* Copyright (c) 2003, 2004, 2005 The Regents of the University of Michigan, Trustees of Indiana University,
-*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
-* 
-* Licensed under the Educational Community License Version 1.0 (the "License");
-* By obtaining, using and/or copying this Original Work, you agree that you have read,
-* understand, and will comply with the terms and conditions of the Educational Community License.
-* You may obtain a copy of the License at:
-* 
-*      http://cvs.sakaiproject.org/licenses/license_1_0.html
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-**********************************************************************************/
+ *
+ * $Id$
+ *
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005 The Regents of the University of Michigan, Trustees of Indiana University,
+ *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License");
+ * By obtaining, using and/or copying this Original Work, you agree that you have read,
+ * understand, and will comply with the terms and conditions of the Educational Community License.
+ * You may obtain a copy of the License at:
+ * 
+ *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **********************************************************************************/
 package  org.sakaiproject.component.common.edu.coursemanagement.test;
 
 import java.util.Date;
@@ -30,6 +30,8 @@ import java.util.List;
 import java.util.Set;
 import org.sakaiproject.component.common.edu.coursemanagement.SessionImpl;
 import org.sakaiproject.component.common.edu.coursemanagement.SessionTypeImpl;
+import org.sakaiproject.component.common.edu.coursemanagement.CanonicalCourseImpl;
+import org.sakaiproject.component.common.edu.coursemanagement.CanonicalCourseStatusTypeImpl;
 
 
 import junit.framework.Assert;
@@ -40,26 +42,30 @@ import junit.framework.Assert;
  * @author <a href="mailto:jholtzman@berkeley.edu">Josh Holtzman</a>
  */
 public class CourseManagerTest extends CourseManagementTestBase {
-
-    protected void onSetUpInTransaction() throws Exception {
-        super.onSetUpInTransaction();
-    }
-    /**
-    public void testCreateCanonicalCourse() throws Exception {
-        courseManagementManager.createCanonicalCourse("title","description","1052-math-101-01",
-	    "*uuid_math-101-01", null);
-        setComplete();
-    }
-    */
-
-    public void testCreateSession() throws Exception {
-        SessionTypeImpl type = new SessionTypeImpl();
-        type.setUuid("*uuid_sessionType_uuid");
-        Date currentDate = new Date();
-        SessionImpl s = (SessionImpl)courseManagementManager.createSession("Fall 2005","1052","2005",
-	    type, "*uuid_"+currentDate.getTime(), Boolean.TRUE, "site", currentDate, "site", currentDate);
-        setComplete();
-        System.out.println("***sessionId = "+s.getSessionId());
-    }
-
+	
+	protected void onSetUpInTransaction() throws Exception {
+		super.onSetUpInTransaction();
+	}
+	public void testCreateCanonicalCourse() throws Exception {
+		Date currentDate = new Date();
+		CanonicalCourseStatusTypeImpl type = (CanonicalCourseStatusTypeImpl)courseManagementManager.
+		getCanonicalCourseStatusType("canonical.active");
+		CanonicalCourseImpl c = (CanonicalCourseImpl) courseManagementManager.createCanonicalCourse(
+				"title","description","1052-math-101-01",
+				"*uuid_"+currentDate.getTime(), type);
+		setComplete();
+		System.out.println("***canonical course Id = "+c.getCanonicalCourseId());
+	}
+	
+	public void testCreateSession() throws Exception {
+		Date currentDate = new Date();
+		SessionTypeImpl type = (SessionTypeImpl)courseManagementManager.getSessionType(
+		"term.spring");
+		SessionImpl s = (SessionImpl)courseManagementManager.createSession(
+				"Fall 2005","1052","2005", type, 
+				"*uuid_"+currentDate.getTime(), Boolean.TRUE);
+		setComplete();
+		System.out.println("***sessionId = "+s.getSessionId());
+	}
+	
 }
