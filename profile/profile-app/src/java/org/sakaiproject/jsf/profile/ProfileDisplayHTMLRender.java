738ee13cd6d385dbf37f6139d63ebf9d190e71f7
rshastri@iupui.edu <rshastri@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-08-22 22:04:01 +0000

    SAK-1772 WYSIWYG Editor drops question mark
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@1330 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile/profile-app/src/java/org/sakaiproject/jsf/profile/ProfileDisplayHTMLRender.java
+++ profile/profile-app/src/java/org/sakaiproject/jsf/profile/ProfileDisplayHTMLRender.java
@@ -1,25 +1,25 @@
 /**********************************************************************************
-* $URL$
-* $Id$
-***********************************************************************************
-*
-* Copyright (c) 2003, 2004, 2005 The Regents of the University of Michigan, Trustees of Indiana University,
-*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
-* 
-* Licensed under the Educational Community License Version 1.0 (the "License");
-* By obtaining, using and/or copying this Original Work, you agree that you have read,
-* understand, and will comply with the terms and conditions of the Educational Community License.
-* You may obtain a copy of the License at:
-* 
-*      http://cvs.sakaiproject.org/licenses/license_1_0.html
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-**********************************************************************************/
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005 The Regents of the University of Michigan, Trustees of Indiana University,
+ *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License");
+ * By obtaining, using and/or copying this Original Work, you agree that you have read,
+ * understand, and will comply with the terms and conditions of the Educational Community License.
+ * You may obtain a copy of the License at:
+ * 
+ *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **********************************************************************************/
 package org.sakaiproject.jsf.profile;
 
 import java.io.IOException;
@@ -29,35 +29,32 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.render.Renderer;
 
+public class ProfileDisplayHTMLRender extends Renderer
+{
+  public boolean supportsComponentType(UIComponent component)
+  {
+    return (component instanceof org.sakaiproject.jsf.profile.ProfileDisplayHTMLComponent);
+  }
 
-public class ProfileDisplayHTMLRender extends Renderer {
-    public boolean supportsComponentType(UIComponent component) {
-        return (component instanceof org.sakaiproject.jsf.profile.ProfileDisplayHTMLComponent);
+  public void encodeBegin(FacesContext context, UIComponent component)
+      throws IOException
+  {
+    if (!component.isRendered()) return;
+    ResponseWriter writer = context.getResponseWriter();
+
+    String value = (String) component.getAttributes().get("value");
+
+    if ((value != null) && (!value.equals("")))
+    {
+      value = value.replaceAll("<strong>", "<b>");
+      value = value.replaceAll("</strong>", "</b>");
+      writer.write(value);
     }
+  }
 
-    public void encodeBegin(FacesContext context, UIComponent component)
-        throws IOException {
-        ResponseWriter writer = context.getResponseWriter();
+  public void encodeEnd(FacesContext context, UIComponent component)
+      throws IOException
+  {
 
-        String value = (String) component.getAttributes().get("value");
-
-        if ((value != null) && (!value.equals(""))) {
-//          int pos;
-//          while ((pos = value.indexOf('?')) != -1){
-//             value = value.substring(0,pos) + " " + value.substring(pos+1);
-//          }
-            value = value.replaceAll("<strong>", "<b>");
-            value = value.replaceAll("</strong>", "</b>");
-            writer.write(value);
-        }
-    }
-
-    public void encodeEnd(FacesContext context, UIComponent component)
-        throws IOException {
-        ResponseWriter writer = context.getResponseWriter();
-
-        String value = (String) component.getAttributes().get("value");
-    }
+  }
 }
-
-
