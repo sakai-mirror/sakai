b31b6a721c89737198327c8d92dfd7a680f100eb
ggolden@umich.edu <ggolden@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-12-12 21:25:50 +0000

    SAK-3123 - updated Sakai conversion with latest gradebook conversion SQL
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@4552 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- docs/updating/sakai_2_0-2_1_oracle_conversion.sql
+++ docs/updating/sakai_2_0-2_1_oracle_conversion.sql
@@ -15,6 +15,7 @@
 -- from file "gradebook/component/src/sql/oracle/sakai_gradebook_2.0.1_to_2.1.sql"
 -- Gradebook related tables changes needed between Sakai 2.01 and 2.1
 alter table GB_GRADABLE_OBJECT_T add (NOT_COUNTED number(1,0));
+update GB_GRADABLE_OBJECT_T set NOT_COUNTED=0 where NOT_COUNTED is NULL and POINTS_POSSIBLE is not NULL;
 ----------------------------------------------------------------------------------------------------------------------------------------
 
 
