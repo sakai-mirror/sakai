b38873bbf797f592c65cce8d2e84257e1b4e3da1
jlannan@iupui.edu <jlannan@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-18 18:33:30 +0000

    Generated DB conversion scripts from 2.1.0 -> 2.1.1 SAK-3618 http://bugs.sakaiproject.org/jira/browse/SAK-3618
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@5421 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ docs/updating/sakai_2_1_0-2_1_1_oracle_conversion.sql
@@ -0,0 +1,26 @@
+-- This is the MySQL Sakai 2.1.0 -> 2.1.1 conversion script
+----------------------------------------------------------------------------------------------------------------------------------------
+--
+-- use this to convert a Sakai database from 2.1.0 to 2.1.1.  Run this before you run your first app server.
+-- auto.ddl does not need to be enabled in your app server - this script takes care of all new TABLEs, changed TABLEs, and changed data.
+--
+----------------------------------------------------------------------------------------------------------------------------------------
+
+
+-- Gradebook
+ALTER TABLE GB_GRADE_RECORD_T ADD UNIQUE (GRADABLE_OBJECT_ID, STUDENT_ID);
+
+-- RWiki
+create table rwikipagetrigger (id varchar2(36) not null, userid varchar2(64) not null, pagespace varchar2(255), pagename varchar2(255), lastseen date, triggerspec clob, primary key (id));
+create table rwikipagemessage (id varchar2(36) not null, sessionid varchar2(255), userid varchar2(64) not null, pagespace varchar2(255), pagename varchar2(255), lastseen date, message clob, primary key (id));
+create table rwikipagepresence (id varchar2(36) not null, sessionid varchar2(255), userid varchar2(64) not null, pagespace varchar2(255), pagename varchar2(255), lastseen date, primary key (id));
+create table rwikipreference (id varchar2(36) not null, userid varchar2(64) not null, lastseen date, preference clob, primary key (id));
+
+
+-- SAM
+ALTER TABLE SAM_PUBLISHEDFEEDBACK_T ADD FEEDBACKAUTHORING integer;
+ALTER TABLE SAM_ASSESSFEEDBACK_T ADD FEEDBACKAUTHORING integer;
+UPDATE SAM_ASSESSFEEDBACK_T SET FEEDBACKAUTHORING = 1 WHERE ASSESSMENTID = 1;
+
+
+
