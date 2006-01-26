f0052728defc1068d3e9cdd67ee2d61541083195
ggolden@umich.edu <ggolden@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-26 04:35:12 +0000

    fix the header
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@5555 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- admin-tools/su/src/java/org/sakaiproject/tool/su/SuTool.java
+++ admin-tools/su/src/java/org/sakaiproject/tool/su/SuTool.java
@@ -1,23 +1,23 @@
 /**********************************************************************************
-* $URL: https://source.sakaiproject.org/svn/trunk/sakai/legacy/component/src/java/org/sakaiproject/component/framework/email/TestEmailService.java $
-* $Id: TestEmailService.java 3831 2005-11-14 20:17:24Z ggolden@umich.edu $
-***********************************************************************************
-*
-* Copyright (c) 2005, 2006 The Sakai Foundation.
-* 
-* Licensed under the Educational Community License, Version 1.0 (the "License"); 
-* you may not use this file except in compliance with the License. 
-* You may obtain a copy of the License at
-* 
-*      http://www.opensource.org/licenses/ecl1.php
-* 
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-* See the License for the specific language governing permissions and 
-* limitations under the License.
-*
-**********************************************************************************/
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
 package org.sakaiproject.tool.su;
 
