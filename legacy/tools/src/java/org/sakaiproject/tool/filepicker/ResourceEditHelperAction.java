510b34440b6b23e363e58263577aac7559586977
john.ellis@rsmart.com <john.ellis@rsmart.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-26 05:36:40 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-3138 SAK-3138 adding the new tags to the wrapper
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@5556 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- legacy/tools/src/java/org/sakaiproject/tool/filepicker/ResourceEditHelperAction.java
+++ legacy/tools/src/java/org/sakaiproject/tool/filepicker/ResourceEditHelperAction.java
@@ -41,10 +41,13 @@ public class ResourceEditHelperAction extends FilePickerAction {
 
    protected String initHelperAction(VelocityPortlet portlet, Context context, RunData rundata,
                                      SessionState sstate, ToolSession toolSession) {
+      sstate.setAttribute(ResourcesAction.STATE_ATTACH_LINKS, Boolean.TRUE.toString());
+      sstate.setAttribute(ResourcesAction.STATE_MODE, ResourcesAction.MODE_HELPER);
       if (toolSession.getAttribute(ResourceEditingHelper.ATTACHMENT_ID) != null) {
          sstate.setAttribute(ResourcesAction.STATE_RESOURCES_MODE, ResourcesAction.MODE_ATTACHMENT_EDIT_ITEM);
          sstate.setAttribute(ResourcesAction.STATE_ATTACH_ITEM_ID,
                toolSession.getAttribute(ResourceEditingHelper.ATTACHMENT_ID));
+         sstate.setAttribute(ResourcesAction.STATE_STRUCTOBJ_TYPE_READONLY, Boolean.TRUE.toString());
       }
       else {
          // must be create
@@ -58,13 +61,13 @@ public class ResourceEditHelperAction extends FilePickerAction {
             if (createSubType != null) {
                sstate.setAttribute(ResourcesAction.STATE_STRUCTOBJ_TYPE,
                      createSubType);
+               sstate.setAttribute(ResourcesAction.STATE_STRUCTOBJ_TYPE_READONLY, Boolean.TRUE.toString());
             }
          }
 
          sstate.setAttribute(ResourcesAction.STATE_CREATE_NUMBER, new Integer(1));
          sstate.setAttribute(ResourcesAction.STATE_CREATE_COLLECTION_ID,
                toolSession.getAttribute(ResourceEditingHelper.CREATE_PARENT));
-         sstate.setAttribute(ResourcesAction.STATE_ATTACH_LINKS, Boolean.TRUE.toString());
       }
 
       initMessage(toolSession, sstate);
