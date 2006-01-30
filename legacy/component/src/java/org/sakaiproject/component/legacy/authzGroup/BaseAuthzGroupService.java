4b9e89daf354921f48c3063cd1202db3a9cdeeaa
ggolden@umich.edu <ggolden@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-30 19:53:52 +0000

    SAK-3112 - the sample user and group providers are no longer OOTB enabled.  The components will look for their providers at init() time, so if any components are defined as these providers they will be found (and if none are, that's ok, too).
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@5607 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- legacy/component/src/java/org/sakaiproject/component/legacy/authzGroup/BaseAuthzGroupService.java
+++ legacy/component/src/java/org/sakaiproject/component/legacy/authzGroup/BaseAuthzGroupService.java
@@ -34,6 +34,7 @@ import java.util.Set;
 import java.util.Stack;
 import java.util.Vector;
 
+import org.sakaiproject.api.kernel.component.cover.ComponentManager;
 import org.sakaiproject.api.kernel.function.cover.FunctionManager;
 import org.sakaiproject.api.kernel.session.cover.SessionManager;
 import org.sakaiproject.exception.IdInvalidException;
@@ -267,6 +268,12 @@ public abstract class BaseAuthzGroupService implements AuthzGroupService, Storag
 			FunctionManager.registerFunction(SECURE_REMOVE_AUTHZ_GROUP);
 			FunctionManager.registerFunction(SECURE_UPDATE_AUTHZ_GROUP);
 			FunctionManager.registerFunction(SECURE_UPDATE_OWN_AUTHZ_GROUP);
+
+			// if no provider was set, see if we can find one
+			if (m_provider == null)
+			{
+				m_provider = (GroupProvider) ComponentManager.get(GroupProvider.class.getName());
+			}
 
 			m_logger.info(this + ".init(): provider: " + ((m_provider == null) ? "none" : m_provider.getClass().getName()));
 		}
