7d862a19aa4b47fec571df542c72868f989ae2ff
lance@indiana.edu <lance@indiana.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-02-03 18:54:31 +0000

    svn merge -r 5753:5754 https://source.sakaiproject.org/svn/trunk/sakai/ . U    docs/updating/sakai_2_1_0-2_1_1_mysql_conversion.sql [lance@hiroyuki sakai_2-1-1]$ svn log -r 5754 https://source.sakaiproject.org/svn/trunk/sakai/ . ------------------------------------------------------------------------ r5754 | lance@indiana.edu | 2006-02-03 13:52:02 -0500 (Fri, 03 Feb 2006) | 3 lines
    SAK-3618
    http://bugs.sakaiproject.org/jira/browse/SAK-3618
    Update 2-1-0 to 2-1-1 SQL conversion scripts
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.sakaiproject.org/svn/branches/sakai_2-1-1@5755 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- docs/updating/sakai_2_1_0-2_1_1_mysql_conversion.sql
+++ docs/updating/sakai_2_1_0-2_1_1_mysql_conversion.sql
@@ -36,7 +36,7 @@ INSERT INTO SAM_ASSESSMETADATA_T (ASSESSMENTMETADATAID, ASSESSMENTID, LABEL,
 
 -- SAK-2753 this patch is for those who use mySQL DB and mySQL ConnectorJ  3.1.x. 
 -- updating label and hint fields for True and False questions, from NULL to "". 
---update SAM_ANSWER_T answer set answer.label="" where  answer.itemid in (select itemid from SAM_ITEM_T item where item.itemid = answer.itemid and item.typeid =4  );
---update SAM_ITEM_T item set item.hint="" where  item.typeid =4 ;
---update SAM_PUBLISHEDANSWER_T answer set answer.label="" where  answer.itemid in (select itemid from SAM_PUBLISHEDITEM_T item where item.itemid = answer.itemid and item.typeid =4  );
---update SAM_PUBLISHEDITEM_T item set item.hint="" where  item.typeid =4 ;
+-- update SAM_ANSWER_T answer set answer.label="" where  answer.itemid in (select itemid from SAM_ITEM_T item where item.itemid = answer.itemid and item.typeid =4  );
+-- update SAM_ITEM_T item set item.hint="" where  item.typeid =4 ;
+-- update SAM_PUBLISHEDANSWER_T answer set answer.label="" where  answer.itemid in (select itemid from SAM_PUBLISHEDITEM_T item where item.itemid = answer.itemid and item.typeid =4  );
+-- update SAM_PUBLISHEDITEM_T item set item.hint="" where  item.typeid =4 ;
