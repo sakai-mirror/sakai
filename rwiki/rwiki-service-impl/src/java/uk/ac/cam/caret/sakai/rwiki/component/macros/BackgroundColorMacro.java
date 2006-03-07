f410c62210327488f22b1dd52b0126830b6361e7
andrew@caret.cam.ac.uk <andrew@caret.cam.ac.uk@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-03-07 14:18:03 +0000

    SAK-3715: Re-format verbatim copy of background color macro file
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai@6506 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- rwiki/rwiki-service-impl/src/java/uk/ac/cam/caret/sakai/rwiki/component/macros/BackgroundColorMacro.java
+++ rwiki/rwiki-service-impl/src/java/uk/ac/cam/caret/sakai/rwiki/component/macros/BackgroundColorMacro.java
@@ -1,4 +1,3 @@
-package uk.ac.cam.caret.sakai.rwiki.component.macros;
 /**********************************************************************************
  *
  * $Header$
@@ -21,7 +20,7 @@ package uk.ac.cam.caret.sakai.rwiki.component.macros;
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  **********************************************************************************/
-
+package uk.ac.cam.caret.sakai.rwiki.component.macros;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -35,66 +34,66 @@ import org.radeox.macro.parameter.MacroParameter;
  * FIXME needs localisation. May even need localisable naming!
  * 
  * @author andrew
- *
+ * 
  */
 public class BackgroundColorMacro extends BaseMacro {
 
-		private static final String BACKGROUND_COLOR_PARAM = "bgcolor";
+	private static final String BACKGROUND_COLOR_PARAM = "bgcolor";
 
-		
-		private static final String[] paramDescription = {
-			"0,color: Change the background color of the contents"
-		};
-		
-		private static final String description = "Change the background color of some text";
-		
-		public String getName() {
-			return "bgcolor";
-		}
+	private static final String[] paramDescription = { "0,color: Change the background color of the contents" };
 
-		public String[] getParamDescription() {
-			return paramDescription;
-		}
-		
-		
-		/* (non-Javadoc)
-		 * @see org.radeox.macro.Macro#getDescription()
-		 */
-		public String getDescription() {
-			return description;
-		}
+	private static final String description = "Change the background color of some text";
 
-		public void execute(Writer writer, MacroParameter params) throws IllegalArgumentException, IOException {
-	        writer.write("<span style='");
-	        
-	        String color = params.get(BACKGROUND_COLOR_PARAM);
-	        if (color == null) {
-	        	// Assume we are using attributes
-	        	color = params.get(0);
-	        }
-	                
-	        // Parse color
-	        writer.write(parse(color, "background: "));
-	        writer.write("'>");
-	        if (params.getContent() != null) {
-	            writer.write(params.getContent());
-	        }
-	        writer.write("</span>");
-		}
-
-		private String parse(String color, String cssClass) {
-			if (color == null) return "";
-			
-			// simplest thing remove all (: ; /* */) from the CSS
-			char[] disallowedChars = { ':', ';', '/', '*', '{', '}', '"', '\'', '\\' }; 
-			for (int i = 0; i < disallowedChars.length; i++) {
-				if (color.indexOf(disallowedChars[i]) > 0) {
-					throw new IllegalArgumentException("Color: "  + color + " is not a real CSS color!");
-				}
-			}
-			return cssClass + color + ';';
-		}
-
+	public String getName() {
+		return "bgcolor";
 	}
 
+	public String[] getParamDescription() {
+		return paramDescription;
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.radeox.macro.Macro#getDescription()
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	public void execute(Writer writer, MacroParameter params)
+			throws IllegalArgumentException, IOException {
+		writer.write("<span style='");
+
+		String color = params.get(BACKGROUND_COLOR_PARAM);
+		if (color == null) {
+			// Assume we are using attributes
+			color = params.get(0);
+		}
+
+		// Parse color
+		writer.write(parse(color, "background: "));
+		writer.write("'>");
+		if (params.getContent() != null) {
+			writer.write(params.getContent());
+		}
+		writer.write("</span>");
+	}
+
+	private String parse(String color, String cssClass) {
+		if (color == null)
+			return "";
+
+		// simplest thing remove all (: ; /* */) from the CSS
+		char[] disallowedChars = { ':', ';', '/', '*', '{', '}', '"', '\'',
+				'\\' };
+		for (int i = 0; i < disallowedChars.length; i++) {
+			if (color.indexOf(disallowedChars[i]) > 0) {
+				throw new IllegalArgumentException("Color: " + color
+						+ " is not a real CSS color!");
+			}
+		}
+		return cssClass + color + ';';
+	}
+
+}
