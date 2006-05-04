880a360504a669b263e60c346c03d093481b700c
lance@indiana.edu <lance@indiana.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-05-04 23:04:37 +0000

    removed defunct tag
    git-svn-id: https://source.sakaiproject.org/svn/sakai/tags/archive@9018 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sakai_2-0-0-rc3/sam/src/org/sakaiproject/tool/assessment/data/ifc/authz/QualifierIteratorIfc.java
+++
@@ -1,9 +0,0 @@
-package org.sakaiproject.tool.assessment.data.ifc.authz;
-
-public  interface QualifierIteratorIfc
-    extends java.io.Serializable
-{
-  boolean hasNextQualifier();
-
-  QualifierIfc nextQualifier();
-}
